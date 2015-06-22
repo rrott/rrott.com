9e3843c2ebc0e825fcf085d211e39fbe0f62e7d1
Roman Rott <roman.rott@gmail.com>
2015-06-22 23:44:14 +0300

    regenerated skeleton

--- config.rb
+++ config.rb
@@ -2,6 +2,10 @@
 # Compass
 ###
 
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
@@ -24,9 +28,10 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
 
 ###
 # Helpers
@@ -35,11 +40,6 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
@@ -47,6 +47,10 @@
 #   end
 # end
 
+# Use LiveReload
+activate :livereload
+
+# Compass configuration
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -55,18 +59,29 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  ignore 'images/*.psd'
+  ignore 'stylesheets/lib/*'
+  ignore 'stylesheets/vendor/*'
+  ignore 'javascripts/lib/*'
+  ignore 'javascripts/vendor/*'
+
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  # activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
 
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_path, "/Content/images/"
 end
