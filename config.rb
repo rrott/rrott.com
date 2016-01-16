781373fd6367d7af1afe47287f197cb579921d5c
Roman Rott <roman.rott@gmail.com>
2016-01-17 00:02:38 +0200

    disabled html minimyser

--- config.rb
+++ config.rb
@@ -44,7 +44,7 @@ page "/portfolio.html", :layout => :portfolio
 
 #activate :autoprefixer
 activate :directory_indexes
-activate :minify_html
+#activate :minify_html
 # Build-specific configuration
 configure :build do
   ignore 'images/*.psd'
