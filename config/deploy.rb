f9d537e4d8ed08f0657c7264a70fb569072eb3d2
Roman Rott <roman.rott@gmail.com>
2016-04-02 13:43:33 +0300

    fixed cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,11 @@ lock '3.4.0'
 set :application, 'rrott.com'
 set :repo_url,    'git@gitlab.com:rrott/rrott.com.git'
 set :deploy_to,   '/var/www/rott.org.ua'
-set :log_level, :info
-set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-#before :setup, 'bundler:install'
+#set :log_level, :info
+
+set :rvm_ruby_string, 'ruby-2.3.0'
+set :rvm_binary,      '~/.rvm/bin/rvm'
+set :rvm_type, :user
 
 desc "See uptime on server"
 task :uptime do
@@ -20,9 +22,7 @@ namespace :deploy do
   desc "Build and deploy"
   task :build_site do
     on roles(:all) do
-      execute :rvm do 
-        "bundle middleman build" 
-      end
+      execute :bundle, :exec, "middleman build"
     end
   end
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,8 @@
 server 'rrott.com', user: 'roman', roles: %w{web}
 role :web, %w{roman@rrott.com}
- set :ssh_options, {
-   keys: %w(/home/roman/.ssh/id_rsa),
-   forward_agent: false,
-   auth_methods: %w(publickey)
- }
+set :stage, 'production'
+set :ssh_options, {
+  keys: %w(/home/roman/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(publickey)
+}
