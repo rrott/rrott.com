174bddf04feecbf4e78b64a6dde563804c48343e
Roman Rott <roman.rott@gmail.com>
2016-04-07 23:39:22 +0300

    started adding tests to blog

---
+++ spec/features/blog_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe 'index', type: :feature do
+  before do
+    @app = application
+    visit '/'
+  end
+
+  context 'Menu' do
+
+  end
+
+  context 'Message' do
+
+  end
+
+  context 'main Part' do
+
+  end
+
+  context 'Footer' do
+
+  end
+end
