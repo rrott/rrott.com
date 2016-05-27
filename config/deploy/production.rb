865e8708cdfe958f26354855409d3cb5d64fef84
Roman Rott <roman.rott@gmail.com>
2016-05-26 22:18:26 -0500

    updated ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server 'rrott.com', user: 'roman', roles: %w{web}, primary: true
 set :repository, "build"
 set :use_sudo, false
-set :rvm_ruby_version, '2.3.0'
+set :rvm_ruby_version, '2.3.1'
 set :rvm_roles, [:web]
 
 role :web, %w{roman@rrott.com}
