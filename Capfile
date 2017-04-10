6a5faa150e930ae4b96976f0ae81a3c56907ff57
Roman Rott <roman.rott@gmail.com>
2017-04-10 18:45:52 +0300

    Updated capistrano & disqus

--- Capfile
+++ Capfile
@@ -3,7 +3,9 @@ require 'capistrano/setup'
 require 'capistrano/deploy'
 require 'capistrano/bundler'
 require 'capistrano/console'
+require "capistrano/scm/git"
 require 'rvm1/capistrano3'
 
+install_plugin Capistrano::SCM::Git
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
-server 'rrott.com', user: 'roman', roles: %w{web}, primary: true
+server '46.101.119.187', user: 'roman', roles: %w{web}, primary: true
 set :repository, "build"
 set :use_sudo, false
-set :rvm_ruby_version, '2.3.1'
+set :rvm_ruby_version, '2.3.4'
 set :rvm_roles, [:web]
 
-role :web, %w{roman@rrott.com}
+role :web, %w{roman@46.101.119.187}
 set :ssh_options, {
   keys: %w(~/.ssh/id_rsa),
   forward_agent: false,
