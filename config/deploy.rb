bb71ba4da7d6cfddd58f1ab822a29990b221965e
Roman Rott <roman.rott@gmail.com>
2017-04-10 17:00:08 +0300

    Updated capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.8.0'
 
 set :application, 'rrott.com'
 set :repo_url,    'git@gitlab.com:rrott/rrott.com.git'
 set :deploy_to,   '/var/www/rrott.com'
 #set :log_level, :info
 
-set :rvm_ruby_string, 'ruby-2.3.1'
+set :rvm_ruby_string, 'ruby-2.3.4'
 set :rvm_binary,      '~/.rvm/bin/rvm'
 set :rvm_type, :user
 
